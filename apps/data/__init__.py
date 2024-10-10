cf34d265ddd073c1871ad4d93785e8877e6dd6ed
RyosukeHanaoka <138783421+RyosukeHanaoka@users.noreply.github.com>
2024-10-10 17:55:27 +0900

    変更

--- apps/data/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, session
-from apps.data.extensions import db, migrate  # migrateが適切にインポートされていることを確認
-from flask import Blueprint
-
-def create_app(config_name):
-    app = Flask(__name__)
-    # 必要な設定や拡張機能を設定する
-    return app
